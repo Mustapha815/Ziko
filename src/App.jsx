5e0fc3067571ee8f05775bbca1118bb64fbd8d62
Your Namel <you@example.com>
2025-06-11 01:37:54 +0100

    up samething

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,7 @@ function App() {
   // Generate 8 random 3D-style images from Picsum
   const carouselImages = [
     "/images/img1.jpg",
-    "/images/img2.jpg",
-    "/images/img3.jpg",
-    "/images/img4.jpg",
-    "/images/img5.jpg",
-    "/images/img6.jpg",
-    "/images/img7.jpg",
-    "/images/img8.jpg",
-    "/images/img9.jpg",
-    "/images/img10.jpg"
+
   ];
 
 
