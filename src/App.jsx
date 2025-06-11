7d7dffe36885e99af2c4d1cd63151149e3ae266e
Your Namel <you@example.com>
2025-06-11 01:29:20 +0100

    up samething

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,11 @@ function App() {
     "/images/img3.jpg",
     "/images/img4.jpg",
     "/images/img5.jpg",
-    "/images/img6.jpg"
+    "/images/img6.jpg",
+    "/images/img7.jpg",
+    "/images/img8.jpg",
+    "/images/img9.jpg",
+    "/images/img10.jpg"
   ];
 
 
